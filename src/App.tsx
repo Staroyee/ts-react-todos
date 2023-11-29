05c62e8872f971cdd0cf303c97daff63bf977eea
Staroyee <danmasefield@gmail.com>
2023-11-29 13:42:31 +1030

    Styled the app created todo components and started app layout. Created states for for handling todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import "./App.css";
+import { useState } from "react";
+import InputField from "./components/InputField";
+import { Todo } from "./models/Todo";
 
-function App() {
-  return <>hello world</>;
-}
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+      setTodo("")
+    }
+  };
+
+  console.log(todos);
+
+  return (
+    <>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        {todos.map((t) => (
+          <li>{t.todo}</li>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default App;
